341cffc2d2dac414cab0bc89468ebf08384e0d99
ferreira <ferreira>
2011-07-22 14:31:09 +0000

    Added Copyright comment

--- facedetector-and-id.cpp
+++ facedetector-and-id.cpp
@@ -1,3 +1,27 @@
+//$Id: facedetector-and-id.cpp,v 1.3 2011/07/22 14:31:09 ferreira Exp $
+
+// Copyright (C) 2008-2011 INESC ID Lisboa.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+
+//$Log: facedetector-and-id.cpp,v $
+//Revision 1.3  2011/07/22 14:31:09  ferreira
+//Added Copyright comment
+//
+
 #include <thread>
 #include <FaceDetector.h>
 #include <cstdlib>
